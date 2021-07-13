aa211cf9bb582a80dcc9e628d70b52a4766bf9cc
Nathan Silva Santos <nathan@w16.com.br>
2021-07-13 10:10:09 -0300

    Product Bombom médio

--- src/constants/productList.js
+++ src/constants/productList.js
@@ -7,6 +7,9 @@ import bombom1 from "../assets/images/products/bombom/bombom-1.jpeg";
 import bombom2 from "../assets/images/products/bombom/bombom-2.jpeg";
 import bombom3 from "../assets/images/products/bombom/bombom-3.jpg";
 
+import bombom4 from "../assets/images/products/bombom/bombom-4.jpg";
+import bombom5 from "../assets/images/products/bombom/bombom-5.jpg";
+
 import barra1 from "../assets/images/products/barra/barra-1.jpeg";
 import barra2 from "../assets/images/products/barra/barra-2.jpeg";
 import barra3 from "../assets/images/products/barra/barra-3.jpeg";
@@ -24,7 +27,7 @@ const productList = [
     images: [palha1, palha2, palha3, palha4],
   },
   {
-    name: "Bombom",
+    name: "Bombom grande",
     price: 12,
     description: () => (
       <div className="flavour-list">
@@ -39,6 +42,22 @@ const productList = [
       </div>
     ),
     images: [bombom1, bombom2, bombom3],
+  },
+  {
+    name: "Bombom médio 2un",
+    weight: 160,
+    price: 15,
+    description: () => (
+      <div className="flavour-list">
+        <div className="flavour-list__title">Sabores disponíveis:</div>
+
+        <ul>
+          <li>Brigadeiro</li>
+          <li>Cookies</li>
+        </ul>
+      </div>
+    ),
+    images: [bombom4, bombom5],
   },
   {
     name: "Barra de Chocolate Gourmet",
